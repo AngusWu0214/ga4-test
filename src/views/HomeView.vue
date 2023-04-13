5f5d47dce3d95cc0c4021e9d53b852582b349d4c
ml-angus <angus.za@medialand.tw>
2023-04-13 15:40:35 +0800

    feat : test CI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div>Welcome Red</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
