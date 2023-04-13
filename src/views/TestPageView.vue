6459c7456e84daab7593e6cdac3842e758dfd629
ml-angus <angus.za@medialand.tw>
2023-04-13 16:23:28 +0800

    feat : add pageview tracker

---
+++ src/views/TestPageView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>測試 ga4 pageview</h1>
+    <button>測試用Click</button>
+  </div>
+</template>
+<script>
+export default {
+    mounted(){
+        console.log('test')
+        this.$gtag.pageview("/about");
+    }
+}
+</script>
